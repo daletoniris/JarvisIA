f9bf93c556840317864039f0d9f02b47e8aaccb2
Daniel Dieser (daletoniris) <daletoniris@gmail.com>
2018-09-14 21:41:51 -0300

    Update jarvispoc.py

--- jarvispoc.py
+++ jarvispoc.py
@@ -63,33 +63,36 @@ if __name__ == "__main__":
     entity, value = wit_response(messaging_text)
     
     if entity == 'saludojarvis':
+         #coloca aquí tu nombre
          print ("hola, daniel como estas?")
          os.system("google_speech -l es 'hola daniel, como estas?' -e speed 1 pitch -600")
     elif entity == 'estadodeanimobien':
           
-          os.system("google_speech -l es 'me alegro que te encuentres bien daniel' -e speed 1 pitch -600")
+          os.system("google_speech -l es 'me alegro que te encuentres bien' -e speed 1 pitch -600")
     elif entity == 'lucesazules':
            print ("modo luz azul activado")                      
            os.system("google_speech -l es 'modo luz azul activado' -e speed 1 pitch -600")
-           os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/celeste.py")
+           os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/celeste.py")
     elif entity == 'lucesverdes':
            print ("modo luz verde activado")
            os.system("google_speech -l es 'modo verde activado' -e speed 1 pitch -600")
-           os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/verde.py")
+           os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/verde.py")
 
     elif entity == 'lucesdescanso':
            print ("modo luz descanso activado")
            os.system("google_speech -l es 'modo luz descanso activado' -e speed 1 pitch -600")
-           os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/naranja.py")
+           os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/naranja.py")
 
     elif entity == 'lucesrojas':
            print ("modo luz roja activado")
            os.system("google_speech -l es 'modo luz roja activado' -e speed 1 pitch -600")
-           os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/rojo.py")
+           os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/rojo.py")
     elif entity == 'lucespurpura':
            print ("modo luz purpura activado")
            os.system("google_speech -l es 'modo luz purpura activado' -e speed 1 pitch -600")
-           os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/purpura.py")
+           #esta linea la comentas si estás corriendo el codigo en una raspberry pi y descomentas la de abajo.
+           os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/purpura.py")
+           os.system("python purpura.py")
     elif entity == 'estadojarvis':
          print ("muy bien por, con mucho animo")
          os.system("google_speech -l es 'muy bien!! con mucho animo!' -e speed 1 pitch -600")
@@ -111,7 +114,7 @@ if __name__ == "__main__":
          os.system(('python3 youtube.py {} ') + (text))
          
     elif entity == 'tempambiente':
-          os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/DHT11_Python/dht11_example.py")
+          os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/DHT11_Python/dht11_example.py")
     elif entity == 'wikipedia_search_query':
          
          os.system(('python3 wiki.py {} ') + (text))
@@ -125,7 +128,9 @@ if __name__ == "__main__":
     elif entity == 'aguamate':
          
          os.system("google_speech -l es 'calentando agua para mate' -e speed 1 pitch -600")
-         os.system("ssh pi@192.168.1.106 python /home/pi/SpeakPy/mate.py")
+        #esta linea la comentas si estás corriendo el codigo en una raspberry pi y descomentas la de abajo.
+         os.system("ssh pi@dir ip de tu raspberry pi python /home/pi/SpeakPy/mate.py")
+         #os.system('python3 mate.py ') 
          os.system("google_speech -l es 'agua lista para mate' -e speed 1 pitch -600")
     else:
       print ("Intente hablar nuevamente...")
